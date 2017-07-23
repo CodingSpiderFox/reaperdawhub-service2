d36043ce76ba63bdc3b6e1b2c443f938ed46b53f
CodingSpiderFox <sunrisechain@gmail.com>
2017-07-23 20:08:44 +0200

    generated jhipster app

---
+++ src/main/java/org/codingspiderfox/reaperdawhub/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package org.codingspiderfox.reaperdawhub.config.audit;
---
+++ src/main/java/org/codingspiderfox/reaperdawhub/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package org.codingspiderfox.reaperdawhub.config;
